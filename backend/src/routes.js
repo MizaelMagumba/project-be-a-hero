7269d632fc3fb35e03239b0e37f15c8046328baa
MizaelMagumba <mizaellmagumba@hotmail.com>
2020-09-30 20:17:50 +0200

    Adicionada funcionalidade insertImage

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,6 +5,8 @@ const CaseController = require('./controllers/CaseController');
 const LoginController = require('./controllers/LoginController');
 const PerfilController = require('./controllers/PerfilController');
 
+const multer = require('multer');
+const multerConfig = require('./config/multer'); 
 
 routes.post('/login', LoginController.login);
 
@@ -17,6 +19,8 @@ routes.post('/company', CompanyController.create);
 routes.get('/casos', CaseController.index);
 routes.post('/casos', CaseController.create);
 routes.delete('/casos/:id', CaseController.delete);
+routes.put('/casos/:id', multer(multerConfig).single("file"), CaseController.upload);
+ 
 
 
 module.exports = routes; 
